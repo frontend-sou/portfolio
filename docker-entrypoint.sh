5587d31e7dd296b88d01119b02f82f12f4bb0566
Nguyen Duc Long <127084307+longndwanosoft@users.noreply.github.com>
2024-07-14 14:10:04 +0700

    update dockerentry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ cd /var/www/html
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate
+php artisan migrate --force
 service nginx start
 php-fpm
 # nginx -g daemon-off
